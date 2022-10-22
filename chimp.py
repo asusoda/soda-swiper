ae4029a584fdf6204a79c3753d60c1fd443cff20
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-22 17:58:44 +0000

    Adding tarfile member sanitization to extractall()

--- chimp.py
+++ chimp.py
@@ -209,7 +209,26 @@ class ChimpRequester(object):
         big_member_dict = {}
         logging.debug("Processing raw_members")
         with tarfile.open('raw_members.tar.gz') as t:
-            t.extractall('./raw_json_members')
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
+                
+            
+            safe_extract(t, "./raw_json_members")
         json_files = glob.glob('./raw_json_members/*.json')
         big_member_dict = {}
         for j_f in json_files:
